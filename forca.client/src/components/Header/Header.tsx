c4bca95b5fc4ffde4a25ce3f16e36a72d9e153f8
jeann <jeannguilherme30@gmail.com>
2025-05-09 17:27:49 -0300

    Teste

--- forca.client/src/components/Header/Header.tsx
+++ forca.client/src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ import styles from './Header.module.css'
 const Header = () => {
   return (
     <header className={styles.header}>
-      <Link to="/calendar" className={styles.logo}>FORCA PROD 2</Link>
+      <Link to="/calendar" className={styles.logo}>FORCA PROD</Link>
     </header>
   )
 }
